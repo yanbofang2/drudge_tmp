108940b91e2e96041229224e62852aacf00e99b9
Guo Chen <peizhi.chen@gmail.com>
2025-09-01 22:05:49 -0500

    Start to migrate to `pyproject.toml`

--- setup.py
+++ setup.py
@@ -4,19 +4,6 @@ import os.path
 
 from setuptools import setup, find_packages, Extension
 
-with open('README.rst', 'r') as readme:
-    DESCRIPTION = readme.read()
-
-CLASSIFIERS = [
-    'Development Status :: 1 - Planning',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Topic :: Scientific/Engineering :: Mathematics'
-]
-
 PROJ_ROOT = os.path.dirname(os.path.abspath(__file__))
 INCLUDE_DIRS = [
     '/'.join([PROJ_ROOT, i])
@@ -39,22 +26,7 @@ wickcore = Extension(
 )
 
 setup(
-    name='drudge',
-    version='0.10.0dev0',
-    description=DESCRIPTION.splitlines()[0],
-    long_description=DESCRIPTION,
-    url='https://github.com/tschijnmo/drudge',
-    author='Jinmo Zhao and Gustavo E Scuseria',
-    author_email='tschijnmotschau@gmail.com',
-    license='MIT',
-    classifiers=CLASSIFIERS,
     packages=find_packages(),
     ext_modules=[canonpy, wickcore],
     package_data={'drudge': ['templates/*']},
-    install_requires=['sympy', 'ipython', 'Jinja2', 'pyspark'],
-    entry_points={
-        'console_scripts': [
-            'drudge = drudge.drs:main'
-        ]
-    }
 )
